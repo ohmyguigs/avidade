5da7b2af62aae3eef64310ca4bda5b273149a1cd
resendefreitas <guilherme.freitas@vtex.com.br>
2016-10-14 03:33:48 -0300

    Fix webpack warnings

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: '#cheap-module-eval-source-map',
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
