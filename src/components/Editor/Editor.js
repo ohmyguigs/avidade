30867100424dcaa68c1e59119c7aeb3b7cb2c1b7
resendefreitas <guilherme.freitas@vtex.com.br>
2016-10-15 20:00:03 -0300

    Editor funcionality working

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -16,6 +16,14 @@ class Editor extends React.Component {
     this.setState({backgroundColor: color.hex.toString()});
   }
 
+  nameChange = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  postChange = (e) => {
+    this.setState({post: e.target.value});
+  }
+
   render() {
     const colors = ['#FFB840','#FF925B','#FF695B','#4BBF51','#48B895','#32ACD0','#E461DF','#9C70D4','#5A8EDF'];
 
@@ -26,9 +34,14 @@ class Editor extends React.Component {
                 name={this.state.name}
                 post={this.state.post}
                 backgroundColor={this.state.backgroundColor} />
+          <CirclePicker className="Editor__picker" onChange={this.colorChange.bind(this)} colors={colors} width="220"  />
+          <div className="Editor__inputs">
+            Robson: <input value={this.state.name} onChange={this.nameChange} type="text" className="Editor__inputs--nome" />
+            Vida:
+            <textarea cols="15" rows="5" value={this.state.post} onChange={this.postChange} type="text" className="Editor__inputs--post">
+            </textarea>
+          </div>
         </div>
-        <hr/>
-        <CirclePicker onChange={this.colorChange.bind(this)} colors={colors} width="220"  />
       </div>
     );
   };
